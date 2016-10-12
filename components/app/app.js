45e043e0e5887d581cc5f04db066b3ddb346ea03
Georgy Perepecho <horprogs@gmail.com>
2016-10-13 00:55:49 +0300

    React init

--- components/app/app.js
+++ components/app/app.js
@@ -1,18 +1,106 @@
 (function () {
     'use strict';
 
-    // import
-    let Ajax = window.Ajax,
-        Skills = window.Skills,
-        SkillItem = window.SkillItem;
+    let ReactDOM = window.ReactDOM,
+        React = window.React,
+        Model = window.Model;
 
-    window.skills = new Skills();
-    //window.SkillItem = new SkillItem();
+    class TooltipLevel extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                skillListData: Model.skillListData,
+                levels: Model.levels
+            }
+        }
 
-    //
-    //new Menu({
-    //    el: document.querySelector('.js-menu'),
-    //    data: menuData
-    //});
+        changeLevel(i, el) {
+            let arr = this.state.skillListData.slice();
+            arr[i].level = el;
 
+            this.setState({
+                skillListData: arr
+            })
+            //я понял что так работать не будет
+        }
+
+        render() {
+            let list = this.state.levels.map((el, i) => {
+                return (
+                    <div className="skills-tooltip__item"
+                         key={i}
+                         onClick={this.changeLevel.bind(this, i, el)}>{el}</div>
+                )
+            });
+            return (
+                <div className="skills-tooltip">
+                    {list}
+                </div>
+            );
+        }
+    }
+
+    class SkillItem extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                isShow: false
+            }
+        }
+
+        showTooltip() {
+            this.setState({
+                isShow: !this.state.isShow
+            })
+        }
+
+        render() {
+            return (
+                <div className="skills__item">
+                    <span className="skills__title">{this.props.title}</span>
+                    <span className="skills__level js-level">{this.props.level}</span>
+                    <a href="#" className="skills__change js-change"
+                       onClick={this.showTooltip.bind(this)}>Edit
+                        {this.state.isShow ? <TooltipLevel/> : null}
+                    </a>
+                    <a href="#" className="skills__change js-info">Info</a>
+                    <a href="#" className="skills__delete js-delete">Delete</a>
+                </div>
+            )
+        }
+    }
+
+    class SkillList extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                skillListData: Model.skillListData
+            }
+        }
+
+        getList() {
+            return this.state.skillListData.map(el => {
+                return (
+                    <SkillItem key={el.id}
+                               title={el.title}
+                               level={el.level}
+                    />
+                )
+            })
+        }
+
+        render() {
+            let list = this.getList();
+            return (
+                <div className="skills">
+                    {list}
+                </div>
+            )
+        }
+    }
+
+    ReactDOM.render(
+        <SkillList/>,
+        document.getElementById('root')
+    );
 })();
--- components/model/model.js
+++ components/model/model.js
@@ -3,18 +3,21 @@
 
     class Model {
         constructor() {
-            this.data = [
+            this.skillListData = [
                 {
+                    id: 1,
                     title: 'HTML',
                     level: 'Advanced',
                     info: 'Info HTML'
                 },
                 {
+                    id: 2,
                     title: 'CSS',
                     level: 'Advanced',
                     info: 'Info CSS'
                 },
                 {
+                    id: 3,
                     title: 'JavaScript',
                     level: 'Intermediate',
                     info: 'Info JavaScript'
